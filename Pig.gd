7554c0b0c80df79fd7ea7518ca48e8ade2218ca6
Fernando Guillen <fguillen.mail@gmail.com>
2022-12-08 20:43:13 +0100

    First commit

---
+++ Pig.gd
@@ -0,0 +1,46 @@
+extends Area2D
+
+export(int) var speed = 200
+
+onready var animationPlayer: = $AnimationPlayer
+
+var running = false
+var direction = Vector2.ZERO
+
+func _process(delta):
+	running = false
+	direction = Vector2.ZERO
+
+	if Input.is_action_pressed("ui_left"):
+		direction = Vector2(-1, 0)
+
+	if Input.is_action_pressed("ui_right"):
+		direction = Vector2(1, 0)
+
+	if Input.is_action_pressed("ui_down"):
+		direction = Vector2(0, 1)
+
+	if Input.is_action_pressed("ui_up"):
+		direction = Vector2(0, -1)
+
+	if direction.x > 0:
+		scale.x = 1
+	elif direction.x < 0:
+		scale.x = -1
+
+	if direction.length() > 0:
+		move(direction, delta)
+		animationPlayer.play("Run")
+	else:
+		animationPlayer.play("Idle")
+
+func _on_Pig_area_entered(area:Area2D):
+	if area is Apple:
+		collect_apple(area)
+
+func collect_apple(apple):
+	apple.queue_free()
+
+func move(direction, delta):
+	running = true
+	position += direction * speed * delta
