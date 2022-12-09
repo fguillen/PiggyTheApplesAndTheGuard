c926235d061dd1efd62e79068268eceb1bd5a4d9
Fernando Guillen <fguillen.mail@gmail.com>
2022-12-09 12:19:45 +0100

    Fixed apple spawner position

--- AppleSpawner.gd
+++ AppleSpawner.gd
@@ -3,6 +3,7 @@ extends Node
 onready var timer: = $Timer
 const Apple = preload("res://Apple.tscn")
 var screen_size = Vector2.ZERO
+var rng = RandomNumberGenerator.new()
 
 func _ready():
 	screen_size = get_viewport().get_visible_rect().size
@@ -12,14 +13,14 @@ func spawn_apple():
 	var apple = Apple.instance()
 	add_child(apple)
 	apple.global_position = rand_position()
-	timer.start(rand_range(2, 5))
+	print("Apple position: ", apple.global_position)
+	timer.start(rand_range(0.5, 5.0))
 
 
 func _on_Timer_timeout():
 	spawn_apple()
 
 func rand_position():
-	var rng = RandomNumberGenerator.new()
 	return Vector2(
 		rng.randi_range(0, screen_size.x),
 		rng.randi_range(0, screen_size.y)
